3fec2182b7ec48a141f48d21de06a4d3435753bc
NewEraCracker <neweracracker@gmail.com>
2016-02-23 15:20:59 +0000

    Disable search engine indexing
    +Minor tweaks to force response to 303 status

--- upload/redirect.php
+++ upload/redirect.php
@@ -3,7 +3,7 @@ if(defined('IN_MYBB')) {
 	die('This file is not meant to be run from MyBB.');
 } else {
 	define('IN_MYBB', 1);
-	require_once './global.php';
+	require_once('./global.php');
 }
 
 /*
@@ -13,8 +13,12 @@ if(defined('IN_MYBB')) {
 */
 $mybb->input['url'] = str_replace(dec_to_utf8(8203), '', $mybb->input['url']);
 
+// Prevent search engines from even trying to index this page
+header('X-Robots-Tag: noindex, nofollow', true);
+
+// Redirect only if valid input is entered
 if(preg_match('/^(http|https|ftp|news){1}:\/\/[^\n\r]*$/', $mybb->input['url'])) {
-	header('Location: ' . $mybb->input['url']);
+	header('Location: ' . $mybb->input['url'], true, 303);
 }
 
 die();
