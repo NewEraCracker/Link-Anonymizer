c497bd4245175c4a04b63723e066524fa32267a5
NewEraCracker <neweracracker@gmail.com>
2016-04-08 16:24:55 +0100

    Releasing version 1.5.3

--- upload/inc/plugins/linkanonymizer.php
+++ upload/inc/plugins/linkanonymizer.php
@@ -21,7 +21,7 @@ function linkanonymizer_info()
 		'website'    => 'https://github.com/NewEraCracker/Link-Anonymizer',
 		'author'     => 'NewEraCracker',
 		'authorsite' => 'https://github.com/NewEraCracker',
-		'version'    => '1.5.2',
+		'version'    => '1.5.3',
 		'guid'       => 'ef3f9596c24e4d7ca4f364f74c2fd12e'
 	);
 }
